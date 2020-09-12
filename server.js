86ad6378b0dab77fe06466e8d093fd467f810342
cldaly <clayton.daly91@gmail.com>
2020-09-12 13:20:02 -0400

    switch to express-force-https

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var enforce = require('express-sslify');
+const secure = require('express-force-https');
 var app = express();
 
+app.use(secure);
 app.use(express.static('./dist/SoarWithBarbie'));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.get('/*', function (req, res) {
   res.sendFile('index.html', { root: 'dist/SoarWithBarbie' });
