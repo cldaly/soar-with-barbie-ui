b6de35969933a77d8e4ef950ad76561fd7080396
cldaly <clayton.daly91@gmail.com>
2020-09-12 11:15:01 -0400

    setup coming soon page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComingSoonComponent
   ],
   imports: [
     BrowserModule,
