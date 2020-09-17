51814db9658783630bc54c5387d9f0bc02390457
cldaly <clayton.daly91@gmail.com>
2020-09-17 12:47:31 -0400

    theta page progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ThetaHealingComponent } from './pages/theta-healing/theta-healing.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'theta-healing', component: ThetaHealingComponent },
   { path: '**', redirectTo: '/' }
 ];
 
