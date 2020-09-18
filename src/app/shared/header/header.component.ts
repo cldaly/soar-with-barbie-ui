f216d5cd15863d39398952e7c8f6b72203260495
cldaly <clayton.daly91@gmail.com>
2020-09-17 23:44:23 -0400

    mobile responsive styling

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+
+  menuIsOpen:boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  toggleMenu(){
+    this.menuIsOpen = !this.menuIsOpen;
   }
-
+  handleSelection() {
+    if (this.menuIsOpen) {
+      this.menuIsOpen = false;
+    }
+  }
 }
