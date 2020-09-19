49debe092d347554d1a161b444c24cab85448943
cldaly <clayton.daly91@gmail.com>
2020-09-19 10:30:07 -0400

    about me work

--- src/app/core/email.service.ts
+++ src/app/core/email.service.ts
@@ -6,12 +6,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EmailService {
+  private baseUrl:string = 'https://soar-with-barbie-svc.herokuapp.com';
   headers:HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private http:HttpClient) { }
 
   sendEmail(formData:FormData):Observable<any> {
     console.log('Sending email',formData);
-    return this.http.post<any>('http://localhost:8080/api/email/send-message', formData, {headers:this.headers});
+    return this.http.post<any>(`${this.baseUrl}/api/email/send-message`, formData, {headers:this.headers});
   }
 }
