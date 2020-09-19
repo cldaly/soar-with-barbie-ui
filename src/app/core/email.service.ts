1fa93264a88e1047bb6ffafb3e119078b10f002a
cldaly <clayton.daly91@gmail.com>
2020-09-19 16:48:19 -0400

    update ng project name, and env apiurl

--- src/app/core/email.service.ts
+++ src/app/core/email.service.ts
@@ -2,11 +2,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
-  private baseUrl:string = 'https://soar-with-barbie-svc.herokuapp.com';
+  private baseUrl:string = environment.apiUrl;
   headers:HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private http:HttpClient) { }
