e4cd7cadfc390b5ab7aa0e061e47025c27daf75f
cldaly <clayton.daly91@gmail.com>
2020-11-18 16:21:09 -0500

    dynamically adjust iFrame height

--- src/app/components/scheduler/scheduler.component.ts
+++ src/app/components/scheduler/scheduler.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scheduler.component.css']
 })
 export class SchedulerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public height: String = "0";
+  public loading: boolean;
+  constructor() {
+    this.loading = true;
+    console.log("Constructed");
   }
 
+  ngOnInit(): void {
+    window.addEventListener("message", (event) => {
+      if (typeof event.data == 'string') {
+        let h = event.data.split(":")[1];
+        document.getElementById("scheduler-frame").style.height = h + "px";
+      }
+    });
+  }
+
+  handleLoad(): void {
+    this.loading = false;
+  }
 }
