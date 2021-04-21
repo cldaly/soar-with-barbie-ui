2d9979094859ce322cd8927090f174816068109f
cldaly <clayton.daly91@gmail.com>
2021-04-21 12:24:33 -0400

    remove event listener on destroy

--- src/app/components/scheduler/scheduler.component.ts
+++ src/app/components/scheduler/scheduler.component.ts
@@ -1,29 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-scheduler',
 	templateUrl: './scheduler.component.html',
 	styleUrls: ['./scheduler.component.css']
 })
-export class SchedulerComponent implements OnInit {
+export class SchedulerComponent implements OnInit, OnDestroy {
 	public height: String = "0";
 	public loading: boolean;
-	
+
 	constructor() {
 		this.loading = true;
 		console.log("Constructed");
 	}
 
 	ngOnInit(): void {
-		window.addEventListener("message", (event) => {
-			if (typeof event.data == 'string') {
-				let h = event.data.split(":")[1];
-				document.getElementById("scheduler-frame").style.height = h + "px";
-			}
-		});
+		window.addEventListener("message", this.schedulerListener);
+	}
+
+	private schedulerListener = (event: MessageEvent<any>) => {
+		if (typeof event.data == 'string') {
+			let h = event.data.split(":")[1];
+			document.getElementById("scheduler-frame").style.height = h + "px";
+		}
 	}
 
 	public handleLoad(): void {
 		this.loading = false;
+	}
+
+	ngOnDestroy(): void {
+		window.removeEventListener("message", this.schedulerListener);
 	}
 }
